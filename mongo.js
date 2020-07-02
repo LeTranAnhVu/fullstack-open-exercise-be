16c2c1431fabc5dd81c394816be8cdfd4215b8d9
letrananhvu <letrananhvu>
2020-07-02 23:35:27 +0300

    part3 3.12 - done

---
+++ mongo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.ashww.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
+//
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+if (name === undefined) {
+  Person.find().then(people => {
+    console.log('phonebook:')
+    people.forEach(person => {
+      console.log(person.name + ' ' + person.number)
+    })
+    mongoose.connection.close()
+  })
+}else {
+  const person = new Person({name, number})
+  person.save().then(savedPerson => {
+    console.log(`added ${savedPerson.name} ${savedPerson.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
