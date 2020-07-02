bc0377940be3e4b2169424798fffe760ae8be85c
letrananhvu <letrananhvu>
2020-07-02 20:35:22 +0300

    part3 3.2 - done

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.get('/api/persons', (req, res) => {
 
 
 
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+
+
 const PORT = 3001
 
 app.listen(PORT, () => {
