990976c5d69dd875963b5b446e6479979b425f1d
letrananhvu <letrananhvu>
2020-07-02 20:41:14 +0300

    part3 3.3 - done

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const app = express()
 app.use(express.json())
 
 const persons = [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
+  {
+    'name': 'Arto Hellas',
+    'number': '040-123456',
+    'id': 1
+  },
+  {
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523',
+    'id': 2
+  },
+  {
+    'name': 'Dan Abramov',
+    'number': '12-43-234345',
+    'id': 3
+  },
+  {
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122',
+    'id': 4
+  }
+]
 
 const generateId = () => {
   const maxId = persons.length > 0
@@ -37,13 +37,22 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-
+app.get('/api/persons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const person = persons.find(person => {
+    return person.id === id
+  })
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).json({message: "Not found!"})
+  }
+})
 
 
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
-
 
 
 const PORT = 3001
